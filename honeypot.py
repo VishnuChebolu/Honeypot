37b616e7e2a621ea9951ad7fb6d7cda1f516e49d
VishnuChebolu <cheboluvishnu@gmail.com>
2021-11-10 06:39:34 -0500

    threading almost done

--- honeypot.py
+++ honeypot.py
@@ -1,10 +1,43 @@
-from utilities.ssh.ssh2 import startSSH
-import cowsay
+#! /usr/bin/env/ python3
+from threading import Thread
+import datetime
+import pytz
+from optparse import OptionParser
+# from utilities.ssh.ssh2 import startSSH
 import pyfiglet
-import time
-import threading
-pyfiglet.figlet_format('HoneyPot')
-print("\t\tby Vishnu")
+
+def getTime():
+    return datetime.datetime.now(pytz.timezone('Asia/Kolkata')).strftime("%d %B %Y %H:%M:%S")
+def startSSH():
+    print("SSH should work")
+
+def http():
+    print("HTTP should work after threading")
+parser = OptionParser(conflict_handler="resolve")
 
 
-startSSH()
+parser.add_option('-h',
+                    dest = 'http', 
+                    action = "store_true",
+                    default = False,
+                    help = 'To start HTTP honeypot')
+
+parser.add_option("-s",
+                    action = "store_true",
+                    dest = "ssh",
+                    default = False,
+                    help = "To start SSH honeypot")
+    
+(options, args) = parser.parse_args()
+print(pyfiglet.figlet_format('HoneyPot'))
+print("\t\t\t\tby Vishnu\n\n")
+if (options.ssh and options.http):
+    raise Exception("Not possible to add both services at same time.")
+elif options.ssh:
+    sshThread = Thread(target = startSSH)
+    sshThread.start()
+    print(f'{getTime(): <25} : ssh')
+elif options.http:
+    httpThread = Thread(target = http)
+    httpThread.start()
+    print(f'{getTime(): <25} : http')
