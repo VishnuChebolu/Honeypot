8a1bf6f311fab7be7e74bcc6df9dbb7c15484a12
VishnuChebolu <cheboluvishnu@gmail.com>
2021-11-12 06:45:08 -0500

    run with mysql server started

--- honeypot.py
+++ honeypot.py
@@ -3,13 +3,11 @@ from threading import Thread
 import datetime
 import pytz
 from optparse import OptionParser
-# from utilities.ssh.ssh2 import startSSH
+from utilities.ssh.ssh2 import startSSH
 import pyfiglet
 
 def getTime():
     return datetime.datetime.now(pytz.timezone('Asia/Kolkata')).strftime("%d %B %Y %H:%M:%S")
-def startSSH():
-    print("SSH should work")
 
 def http():
     print("HTTP should work after threading")
@@ -34,9 +32,7 @@ print("\t\t\t\tby Vishnu\n\n")
 if (options.ssh and options.http):
     raise Exception("Not possible to add both services at same time.")
 elif options.ssh:
-    sshThread = Thread(target = startSSH)
-    sshThread.start()
-    print(f'{getTime(): <25} : ssh')
+    startSSH()
 elif options.http:
     httpThread = Thread(target = http)
     httpThread.start()
